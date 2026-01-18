ca16ec445f62b8fad05541e65024ce817e89a41f
Kkhoa-clon <kkhoa8213@gmail.com>
2026-01-18 09:39:15 +0700

    version 2.0

--- frontend/src/pages/VR-AR.tsx
+++ frontend/src/pages/VR-AR.tsx
@@ -5,7 +5,7 @@ const VRAR: React.FC = () => {
   return (
     <Box sx={{ position: 'fixed', top: 65, left: 0, width: '100vw', height: '100vh', zIndex: 0 }}>
       <iframe
-        src="https://vangtien.github.io/VR_THUC-TE-AO/"
+        src="https://vangtien.github.io/VR_THUC-TE-AO"
         width="100%"
         height="100%"
         style={{ border: 'none', display: 'block' }}
